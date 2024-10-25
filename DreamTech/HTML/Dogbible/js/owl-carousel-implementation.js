80c4ce33c7b637a447abfc960b426a12c9e4540f
Sridharj9005 <happybeingdarkevil@gmail.com>
2024-10-25 22:12:36 +0530

    gave background shade to the dog icon.

---
+++ DreamTech/HTML/Dogbible/js/owl-carousel-implementation.js
@@ -0,0 +1,25 @@
+var owl = $(".owl-carousel");
+owl.owlCarousel({
+  autoplay: true,
+  autoplayHoverPause: true,
+  autoplayTimeout: 3000,
+  loop: true,
+  margin: 10,
+  navText: ["<", ">"],
+  responsiveClass: true,
+  responsive: {
+    0: {
+      items: 1,
+      nav: true,
+      dots: false,
+    },
+    600: {
+      items: 3,
+      nav: true,
+    },
+    1000: {
+      items: 4,
+      nav: true,
+    },
+  },
+});

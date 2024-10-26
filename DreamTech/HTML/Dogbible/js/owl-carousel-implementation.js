5b5f7a6e94a6a6ac06018d937ec4f69315e088b7
Sridharj9005 <happybeingdarkevil@gmail.com>
2024-10-26 14:11:35 +0530

    finally completed breeds category with owl carousel.

--- DreamTech/HTML/Dogbible/js/owl-carousel-implementation.js
+++ DreamTech/HTML/Dogbible/js/owl-carousel-implementation.js
@@ -1,22 +1,35 @@
 var owl = $(".owl-carousel");
+
+function createRows() {
+  var itemsPerRow = 2;
+  var allItems = $(".breedicon-box");
+  var groupedItems = [];
+  for (var i = 0; i < allItems.length; i += itemsPerRow) {
+    var rowItems = allItems.slice(i, i + itemsPerRow);
+    var wrapper = $("<div class='item-row'></div>");
+    rowItems.appendTo(wrapper);
+    groupedItems.push(wrapper);
+  }
+  $(".owl-carousel").empty().append(groupedItems);
+}
+createRows();
 owl.owlCarousel({
-  margin: 10,
-  navText: ["<--", "-->"],
+  navText: [
+    "<img src='source/svg/left-arrow.png' alt='Previous' class='custom-nav'>",
+    "<img src='source/svg/right-arrow.png' alt='Next' class='custom-nav'>",
+  ],
   nav: true,
-  dots: true,
+  dots: false,
   slideBy: "page",
   responsive: {
     0: {
       items: 1,
-      rows: 2, //custom option not used by Owl Carousel, but used by the algorithm below
     },
     768: {
-      items: 2,
-      rows: 3, //custom option not used by Owl Carousel, but used by the algorithm below
+      items: 3,
     },
     991: {
       items: 5,
-      rows: 2, //custom option not used by Owl Carousel, but used by the algorithm below
     },
   },
 });

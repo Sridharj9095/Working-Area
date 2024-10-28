f292f681d987bc8a00ce081fe4d5bf6fde698282
Sridharj9005 <happybeingdarkevil@gmail.com>
2024-10-28 20:52:35 +0530

    adding collapsable menu.

---
+++ DreamTech/HTML/Dogbible/js/script.js
@@ -0,0 +1,6 @@
+document.querySelector(".right-menu").addEventListener("click", function () {
+  document.querySelector(".right-col1").style.display = "block";
+});
+document.querySelector(".menu-close").addEventListener("click", function () {
+  document.querySelector(".right-col1").style.display = "none";
+});

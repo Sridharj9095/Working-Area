2f7d3a5cf3ca2ef9a7d573c6310d67e81b073271
Sridharj9005 <happybeingdarkevil@gmail.com>
2024-12-17 17:26:08 +0530

    added active state link property to highlight active navlink using usestate hooks method.

--- DreamTech/React/my-new-app/src/header.js
+++ DreamTech/React/my-new-app/src/header.js
@@ -1,24 +1,62 @@
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+
 const Header = () => {
+  const [activeLink, setActiveLink] = useState("/");
+
+  const handleLinkClick = (path) => {
+    setActiveLink(path);
+  };
+
   return (
     <div className="header">
       <div className="container">
         <h1 className="logo">DreamTech</h1>
         <ul>
           <li>
-            <Link to="/">Home</Link>
+            <Link
+              to="/"
+              className={activeLink === "/" ? "active" : ""}
+              onClick={() => handleLinkClick("/")}
+            >
+              Home
+            </Link>
           </li>
           <li>
-            <Link to="/products">Products</Link>
+            <Link
+              to="/products"
+              className={activeLink === "/products" ? "active" : ""}
+              onClick={() => handleLinkClick("/products")}
+            >
+              Products
+            </Link>
           </li>
           <li>
-            <Link to="/services">Services</Link>
+            <Link
+              to="/services"
+              className={activeLink === "/services" ? "active" : ""}
+              onClick={() => handleLinkClick("/services")}
+            >
+              Services
+            </Link>
           </li>
           <li>
-            <Link to="/about">About</Link>
+            <Link
+              to="/about"
+              className={activeLink === "/about" ? "active" : ""}
+              onClick={() => handleLinkClick("/about")}
+            >
+              About
+            </Link>
           </li>
           <li>
-            <Link to="/contact">Contact Us</Link>
+            <Link
+              to="/contact"
+              className={activeLink === "/contact" ? "active" : ""}
+              onClick={() => handleLinkClick("/contact")}
+            >
+              Contact Us
+            </Link>
           </li>
         </ul>
         <button>Get Started</button>
@@ -26,4 +64,5 @@ const Header = () => {
     </div>
   );
 };
+
 export default Header;
